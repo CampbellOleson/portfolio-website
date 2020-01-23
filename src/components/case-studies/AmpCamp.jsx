c3449f90704ee10d63586aa1f1364be1c11508c9
Campbell Oleson <campbellsoleson@gmail.com>
2020-01-22 22:03:09 -0700

    ampcamp basics done

--- src/components/case-studies/AmpCamp.jsx
+++ src/components/case-studies/AmpCamp.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import Snippet from "../../assets/screenshots/ripcamp/rc-findbybounds.png";
+import Choose from "../../assets/screenshots/ampcamp/ac-choose.png";
+import AlbumShow from "../../assets/screenshots/ampcamp/ac-albumshow.png";
+import Profile from "../../assets/screenshots/ampcamp/ac-profile.png";
+import Search from "../../assets/screenshots/ampcamp/ac-search.png";
+import SignUp from "../../assets/screenshots/ampcamp/ac-signup.png";
+import Upload from "../../assets/screenshots/ampcamp/ac-upload.png";
+import Home from "../../assets/screenshots/ampcamp/ac-home.png";
+import UploadLoop from "../../assets/screenshots/ampcamp/ac-uploadloop.png";
+import UploadError from "../../assets/screenshots/ampcamp/ac-uploaderror.png";
 import "../../stylesheets/case-studies/case-studies.css";
 import "../../stylesheets/case-studies/ampcamp.css";
 
@@ -16,9 +24,9 @@ const AmpCamp = () => {
               <p>September 2019</p>
             </div>
             <summary>
-              I created a map-based surf locator website that allows users to
-              find surf spots around the globe. RipCamp’s primary purpose is to
-              connect people with the surf.
+              I created a heavy-metal themed music streaming site. AmpCamp
+              serves as a platform where musicians can share their music, and
+              listeners can discover new artists.
             </summary>
           </section>
         </div>
@@ -27,19 +35,25 @@ const AmpCamp = () => {
         </div>
       </section>
 
-      {/* <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo">
-        <img src={Splash} alt="RipCamp Splash Page" className="cs-img" />
-      </div> */}
+      <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo">
+        <img src={Home} alt="AmpCamp Home Page" className="cs-img" />
+      </div>
 
       <div className="hor-center">
         <div className="cs-textblock">
           <header>Assignment</header>
           <article>
-            During the later portion of my full-stack web development course
-            with App Academy, two other students and I were tasked with
-            designing and creating a full-stack web application. Being surfing
-            enthusiasts, we came up with the idea of RipCamp: a one-stop-shop
-            for planning your day of surfing.
+            AmpCamp is a full-stack web application that I created as a project
+            at App Academy. I worked on AmpCamp with a team of two other
+            students. We used GitHub for source code management.
+          </article>
+          <article>
+            As our final project with App Academy, my team wanted to take on a
+            sophisticated project. We chose to create a music streaming site
+            because we wanted something with functionality that goes beyond that
+            of an every-day website. Our idea for the site was a platform where
+            artists could share their music, complete with artist profiles,
+            profile photos, and album uploading capability.{" "}
           </article>
         </div>
       </div>
@@ -48,22 +62,24 @@ const AmpCamp = () => {
         <div className="cs-textblock">
           <header>Planning</header>
           <article>
-            Before beginning on RipCamp, my team and I planned out the
-            architecture of the app. We determined that the primary interface
-            would consist of a map and a surf-spots index, allowing users to get
-            information on a surf spots while simultaneously viewing the spots’
-            geographical locations on the map. Furthermore, RipCamp would need
-            to include a search feature so users could locate particular spots.
-            Finally, we decided to include a show page for every surf spot,
-            displaying photos of the spot, as well as information about weather
-            and surf conditions.
+            In AmpCamp, my team tried to emulate the core functionality of band
+            camp. We decided that the home page should be a place where users
+            could discover music, complete with “daily albums” and search
+            functionality.
           </article>
-
           <article>
-            I like to make sure that the projects I work on are organized to
-            make sure that there are no conflicts between features. On this
-            project, my team and I used GitHub to manage version control and
-            Trello to divvy out responsibilities.
+            We also determined that our site should be easy-to-use for artists,
+            with functionality to seamlessly upload entire albums at a time. We
+            determined that artists should have profile pages where all their
+            albums and songs could be viewed and listened to. This meant that we
+            also had to integrate audio-player functionality into our app.
+          </article>
+          <article>
+            Like my other projects at App Academy, AmpCamp is not a
+            production-quality website. It was an opportunity for me and my
+            teammates to learn about more sophisticated facets of web
+            development. That being said, we built a lot of functionality into
+            AmpCamp, and I stand behind all of our work.
           </article>
         </div>
       </div>
@@ -72,125 +88,139 @@ const AmpCamp = () => {
         <div className="cs-textblock cs-bmo">
           <header>Features and Functionality</header>
           <article>
-            We engineered RipCamp with NodeJS on the backend and MongoDB as the
-            database. We chose MongoDB so we could easily store lots of
-            unrelated records with minimal setup.
+            AmpCamp was implemented with NodeJS and MongoDB on the backend. We
+            used the ExpressJS library for our backend server, and tested our
+            API with Postman during development.
           </article>
           <article>
-            We used Redux to manage application state, and designed a responsive
-            user interface using ReactJS. We also integrated Google Maps API for
-            the maps-based interface, Open Weather API for weather conditions,
-            and AWS S3 Bucket for photo hosting.
+            On the front end, we leveraged React for the UI and Apollo GraphQL
+            for requests to our backend API. AmpCamp also integrates AWS SW
+            Bucket for hosting photos and audio files.
           </article>
-          <article>
-            RipCamp’s home page consists of a map-based interface on the right,
-            enabling users to view the geographical location of surf spots, and
-            a list of surf spots on the left, complete with quickshow items that
-            have names, descriptions, and scrolling photos.
-          </article>
-          <article>
-            I used JavaScript DOM manipulation here for some cool UI effects.
-            For example, if you hover your mouse over a surf spot on the map,
-            the list will automatically scroll to and highlight the
-            corresponding quickshow. This makes for smooth navigation and easy
-            discovery of surf spots!
+          <article className="cs-bmo">
+            The homepage of AmpCamp enables users to discover new music via
+            “AmpCamp Daily Picks”, and search for artists, albums, and songs. A
+            play/pause button on the homepage album index items allows users to
+            listen to albums on the home page.
           </article>
         </div>
       </div>
-{/* 
-      <div className="cs-img-wrapper cs-img-margin">
-        <img src={Home} alt="Feed Screenshot" className="cs-img" />
-      </div> */}
+
+      <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo">
+        <img src={Search} alt="AmpCamp Home Page" className="cs-img" />
+      </div>
 
       <div className="hor-center">
         <div className="cs-textblock cs-bmo">
           <article className="cs-bmo">
-            If you click on a surf-spot quickshow item from the list, you will
-            be redirected to a surf-spot show page, where you can view photos of
-            the spot and see the local weather conditions.
+            If you navigate to an artists profile page, you can view all of the
+            albums and songs that an artist has released. Similarly, there is an
+            album show page that allows you to view the details of a particular
+            album. You can stream music on an artists profile page, and a fixed
+            music-player nav bar on the bottom of the page will display the name
+            of the song that’s playing, the album artwork, and the artist that
+            the song belongs to.
           </article>
         </div>
       </div>
 
-      {/* <div className="cs-img-wrapper cs-img-margin">
-        <img src={Show} alt="Feed Screenshot" className="cs-img" />
-      </div> */}
+      <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo cs-bmo">
+        <img src={Profile} alt="AmpCamp Home Page" className="cs-img" />
+      </div>
+
+      <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo">
+        <img src={AlbumShow} alt="AmpCamp Home Page" className="cs-img" />
+      </div>
 
       <div className="hor-center">
         <div className="cs-textblock cs-bmo">
           <article className="cs-bmo">
-            You can also search the name of your favorite surf spot and find its
-            show page, as well as its location on the map. Similarly, you can
-            apply location-based filters to your search, which will recenter the
-            map on a city of your choosing.
+            I was responsible for creating the upload functionality of AmpCamp.
+            If you sign up to AmpCamp as an artist, you can upload as many songs
+            as you want as an album, complete with a description and a cover
+            art. I used React to create a live preview of the album information,
+            so an artist can look over the details of their album before
+            uploading. This page asynchronously uploads audio files to AWS S3
+            Bucket, and then stores the URLs in MongoDB so the tracks can be
+            played back later.
           </article>
         </div>
       </div>
 
-      {/* <div className="cs-img-wrapper cs-img-margin">
-        <img src={Search} alt="Feed Screenshot" className="cs-img" />
-      </div> */}
+      <div className="cs-img-wrapper cs-img-margin cs-tpf cs-tmo cs-bmo">
+        <img src={Upload} alt="AmpCamp Home Page" className="cs-img" />
+      </div>
 
       <div className="hor-center">
-        <div className="cs-textblock cs-bmo">
-          <article>
-            RipCamp’s interface uses the boundaries of the map to query the
-            database for surf spots. This allows users to scroll around the map
-            and see surf spots pop up all over the world! The surf-spots index
-            will also change to reflect whatever spots are currently being
-            displayed on the map.
-          </article>
-          <article>
-            Overall, we produced a fluid map-based UI that allows users to
-            easily navigate the globe and find information about different surf
-            spots. Currently, RipCamp’s database only has seed data, but once it
-            is populated with real surf-spot information, it will prove useful
-            to surfers everywhere.
-          </article>
-        </div>
-      </div>
-
-      {/* <div className="hor-center">
         <div className="cs-img-wrapper cs-img-margin">
-          <img src={SignUp} alt="Feed Screenshot" className="cs-img-small" />
+          <img
+            src={UploadLoop}
+            alt="Upload Error Screenshot"
+            className="cs-img"
+          />
         </div>
-      </div> */}
+      </div>
+
+      <div className="hor-center">
+        <div className="cs-textblock cs-bmo">
+          <article className="cs-bmo">
+            AmpCamp has fully functional upload and streaming capabilities,
+            complete with a fluid and intuitive user interface.
+          </article>
+        </div>
+      </div>
+
+      <div className="hor-center">
+        <div className="cs-img-small-wrapper cs-img-margin">
+          <img src={Choose} alt="Search Screenshot" className="cs-img-small" />
+          <img
+            src={SignUp}
+            alt="Requests Modal Screenshot"
+            className="cs-img-small"
+          />
+        </div>
+      </div>
 
       <div className="hor-center">
         <div className="cs-textblock cs-bmo">
           <header>Challenges</header>
-          <article className="cs-bmo">
-            The biggest development challenge on RipCamp was figuring how to
-            make the surf-spots list display the spots that were currently
-            within the boundaries of the map (and only those spots). While
-            digging through the docs of Google Maps API, I determined that we
-            could use the bounds from the map to query the database for surf
-            spots. I used a JavaScript Regex library to write queries to
-            retrieve all the surf spots with coordinates falling within the
-            range of the bounds.
+          <article>
+            One of the challenges I faced when creating the album upload page on
+            AmpCamp was asynchronous exception handling. Occasionally, albums
+            would fail to upload to S3 part of the way through, leaving the
+            artist with a semi-uploaded album. The way I got around this was to
+            have the app stay on the upload page until the upload was complete,
+            afterwhich, it would redirect to the new album show page. If the
+            upload failed, the local state of the upload page would be preserved
+            (so the user didn’t have to recreate the album) and a user-friendly
+            error message would prompt the user to “try again in a moment”. This
+            taught me the importance of good error handling as it pertains to
+            user experience.
           </article>
         </div>
       </div>
 
-      {/* <div className="hor-center">
+      <div className="hor-center">
         <div className="cs-img-wrapper cs-img-margin">
-          <img src={Snippet} alt="Feed Screenshot" className="cs-img-small" />
+          <img
+            src={UploadError}
+            alt="Requests Modal Screenshot"
+            className="cs-img"
+          />
         </div>
-      </div> */}
+      </div>
 
       <div className="hor-center">
         <div className="cs-textblock">
           <header>Conclusion</header>
           <article>
-            It’s important to note that this project was for educational
-            purposes. There are still elements that could be improved, and not
-            every feature is production quality. RipCamp will need quite a bit
-            of optimization before it’s ready for a real user base. Nonetheless,
-            building RipCamp was a great experience, and it taught me a lot
-            about JavaScript DOM manipulation, application state, and
-            integrating external APIs.
+            The result of our work was a fully functional indie streaming site,
+            with upload and download functionality. I plan to optimize AmpCamp
+            further as time goes on, and I hope to make the audio player
+            compatible with mobile devices as well. All in all, AmpCamp was a
+            great learning experience, and it taught me how to create bigger web
+            apps with more complex functionality.
           </article>
-          <article>Another conclusion paragraph</article>
         </div>
       </div>
     </div>
