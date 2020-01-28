e07904b13c38ea560be4ccf87bfeb59a9c7a7a54
Campbell Oleson <campbellsoleson@gmail.com>
2020-01-28 16:36:36 -0700

    organized styles

--- src/components/main/TopPanel.jsx
+++ src/components/main/TopPanel.jsx
@@ -17,17 +17,19 @@ const TopPanel = () => {
     <div>
       <div id="tp-wrapper">
         <div className="vert-center">
-          <section>
-            <header className="tpt">Hello World!</header>
-            <main className="tpt">
-              I heard you're looking for a web developer.
-            </main>
-            <p className="tpt">
-              If you're into responsive interfaces, modern design, and clean,
-              modular code, then I'm your guy.
-            </p>
-          </section>
-          <img id="profile-photo" src={ProfilePhoto} alt="" />
+          <div className="tp-content">
+            <img id="profile-photo" src={ProfilePhoto} alt="" />
+            <section className="tp-text">
+              <header className="tpt">Hello World!</header>
+              <main className="tpt">
+                I heard you're looking for a web developer.
+              </main>
+              <p className="tpt">
+                If you're into responsive interfaces, modern design, and clean,
+                modular code, then I'm your guy.
+              </p>
+            </section>
+          </div>
         </div>
         <button
           id="scroll-arrow"
